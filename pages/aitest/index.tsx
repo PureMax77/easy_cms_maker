6ad0e2a83729c10cb200c6855d99dbb3e54f6421
ertr <ertr@coinplug.com>
2024-05-20 09:22:45 +0900

    open ai 연결 추가

--- pages/aitest/index.tsx
+++ pages/aitest/index.tsx
@@ -1,26 +1,60 @@
 import { Sandpack } from "@codesandbox/sandpack-react";
+import { Button, Input } from "@nextui-org/react";
+import OpenAI, { ClientOptions } from "openai";
+import { useState } from "react";
 
 export default function AiTest() {
+  const [aiCode, setAiCode] = useState<string>(`export default function App() {
+  return <h1>Hello Sandpack</h1>
+}`);
+  const [userContent, setUserContent] = useState<string>("");
+
+  const getCode = async (content: string) => {
+    const response = await fetch("/api/summarizer", {
+      method: "POST", // HTTP 메소드를 POST로 설정
+      headers: {
+        "Content-Type": "application/json", // 콘텐츠 타입을 JSON으로 지정
+      },
+      body: JSON.stringify({
+        content, // 전송할 데이터
+      }),
+    });
+
+    const result = await response.json();
+    const cleanCode = result.summary.replace(/```typescript|```tsx|```/g, "");
+    console.log(cleanCode);
+    setAiCode(cleanCode);
+    // return completion;
+  };
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    getCode(userContent);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <form
+        className="flex flex-row justify-center w-full mx-auto"
+        onSubmit={onSubmit}
+      >
+        <Input
+          className="mr-2 max-w-screen-md w-full"
+          placeholder="요청사항을 입력하세요."
+          value={userContent}
+          onChange={(e) => setUserContent(e.target.value)}
+        />
+        <Button type="submit">요청</Button>
+      </form>
+
       <Sandpack
-        template="nextjs"
+        template="react-ts"
+        options={{
+          externalResources: ["https://cdn.tailwindcss.com"],
+        }}
         files={{
-          "/pages/index.js": {
-            code: `export default function Home({ data }) {
-  return (
-    <div>
-      <h1>Hello {data}33</h1>
-    </div>
-  );
-}
-  
-export function getServerSideProps() {
-  return {
-    props: { data: "world" },
-  }
-}
-`,
+          "/App.tsx": {
+            code: `${aiCode}`,
           },
         }}
       />
---
+++ pages/api/summarizer.ts
@@ -0,0 +1,62 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import OpenAI, { ClientOptions } from "openai";
+
+type Data = {
+  summary?: string;
+  error?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === "POST") {
+    // 요청 본문에서 데이터 추출
+    const { content: userContent } = req.body;
+    // const caption = await fs.readFileSync("./caption.txt", {
+    //   encoding: "utf8",
+    // });
+
+    const configuration: ClientOptions = {
+      apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+    };
+    const client = new OpenAI(configuration);
+
+    try {
+      console.log("start Summarize");
+      const completion = await client.chat.completions.create({
+        model: "gpt-3.5-turbo-0125", // 사용할 모델을 지정하세요.
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are an assistant who writes React App.tsx code using TailwindCss. You must provide only code, no additional explanation, in your answer.",
+          },
+          {
+            role: "user",
+            content: userContent,
+          },
+        ],
+        temperature: 0.3, // 창의성 표현 0~1
+        max_tokens: 1000,
+        top_p: 0.7, // 다양한 가능성?을 고려하는 정도 0~1
+        frequency_penalty: 0.0, // 자주 반복되는 단어 사용을 줄이기 위해
+        presence_penalty: 0.0, // 이미 나온 내용을 반복하는 것을 줄이기 위해
+      });
+
+      console.log("Finished Summarize");
+      // console.log(completion.choices[0].message);
+
+      res
+        .status(200)
+        .json({ summary: completion.choices[0].message.content || "" });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ error: "OpenAI 요청 중 에러 발생" });
+    }
+  } else {
+    // POST 요청이 아닐 경우, 405 Method Not Allowed 응답
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
