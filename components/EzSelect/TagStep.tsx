8e4d6e358ab844eacf830166b1e6811ae617064e
윤효정 <hjyoon@coinplug.com>
2024-06-05 14:57:08 +0900

    :sparkles: feat: 변경된 사항 저장

--- components/EzSelect/TagStep.tsx
+++ components/EzSelect/TagStep.tsx
@@ -30,69 +30,86 @@ const TagStep: React.FC<Props> = ({ itemKey }) => {
   } = useDisclosure();
 
   return (
-    <div className="flex">
-      <div className="px-5 border-r-1">
-        <span className="mr-3">{itemKey + 1} Section Information</span>
-        <Tabs
-          color="default"
-          radius="sm"
-          onSelectionChange={(e: any) => setTagType(e)}
-        >
-          {tagTypes.map((type) => (
-            <Tab key={type} title={type} />
-          ))}
-        </Tabs>
+    <>
+      <div className="px-3">
+        <div className="flex justify-center items-center">
+          <span className="mr-3 w-[275px] font-medium subTitle">
+            {itemKey + 1} Section Information
+          </span>
+          <input
+            type="text"
+            className="w-full border-1 border-neutral-300 rounded h-[36px]"
+          />
+        </div>
+        <div className="flex mt-2">
+          <Tabs
+            color="success"
+            radius="sm"
+            onSelectionChange={(e: any) => setTagType(e)}
+            className="optTabs"
+          >
+            {tagTypes.map((type) => (
+              <Tab key={type} title={type} />
+            ))}
+          </Tabs>
+          {tagType === EzTagTypes.LIST && (
+            <>
+              <div className="flex px-5 border-r-1">
+                <RadioGroup className="justify-center" orientation="horizontal">
+                  {listLayoutTypes.map((type, index) => (
+                    <Radio key={index} value={type}>
+                      {type}
+                    </Radio>
+                  ))}
+                </RadioGroup>
+              </div>
+              <div className="flex items-center px-5 gap-3">
+                <Checkbox>Icon</Checkbox>
+                <Checkbox>Image</Checkbox>
+                <Checkbox>Drag & Drop</Checkbox>
+              </div>
+            </>
+          )}
+          {tagType === EzTagTypes.TABLE && (
+            <>
+              <div className="flex items-center px-5 border-r-1">
+                <Input
+                  className="w-[70px] h-[34px] mr-1 border-1 border-neutral-300 rounded optNumber"
+                  type="number"
+                />
+                <span className="mr-3">Column</span>
+                <EzTableModal
+                  isOpen={table_isOpen}
+                  onOpen={table_onOpen}
+                  onOpenChange={table_onOpenChange}
+                />
+              </div>
+              <div className="flex items-center px-5 gap-3">
+                <Checkbox>Row Click Event</Checkbox>
+                <Checkbox>Pagination</Checkbox>
+                <Checkbox>Drag & Drop</Checkbox>
+              </div>
+            </>
+          )}
+          {tagType === EzTagTypes.FORM && (
+            <>
+              <div className="flex items-center px-5">
+                <Input
+                  className="w-[70px] h-[34px] mr-1 border-1 border-neutral-300 rounded optNumber"
+                  type="number"
+                />
+                <span className="mr-3">Item</span>
+                <EzTableModal
+                  isOpen={table_isOpen}
+                  onOpen={table_onOpen}
+                  onOpenChange={table_onOpenChange}
+                />
+              </div>
+            </>
+          )}
+        </div>
       </div>
-      {tagType === EzTagTypes.LIST && (
-        <>
-          <div className="flex px-5 border-r-1">
-            <RadioGroup className="justify-center" orientation="horizontal">
-              {listLayoutTypes.map((type, index) => (
-                <Radio key={index} value={type}>
-                  {type}
-                </Radio>
-              ))}
-            </RadioGroup>
-          </div>
-          <div className="flex items-center px-5 gap-3">
-            <Checkbox>Icon</Checkbox>
-            <Checkbox>Image</Checkbox>
-            <Checkbox>Drag & Drop</Checkbox>
-          </div>
-        </>
-      )}
-      {tagType === EzTagTypes.TABLE && (
-        <>
-          <div className="flex items-center px-5 border-r-1">
-            <Input className="mr-1" type="number" />
-            <span className="mr-3">Column</span>
-            <EzTableModal
-              isOpen={table_isOpen}
-              onOpen={table_onOpen}
-              onOpenChange={table_onOpenChange}
-            />
-          </div>
-          <div className="flex items-center px-5 gap-3">
-            <Checkbox>Row Click Event</Checkbox>
-            <Checkbox>Pagination</Checkbox>
-            <Checkbox>Drag & Drop</Checkbox>
-          </div>
-        </>
-      )}
-      {tagType === EzTagTypes.FORM && (
-        <>
-          <div className="flex items-center px-5">
-            <Input className="mr-1" type="number" />
-            <span className="mr-3">Item</span>
-            <EzTableModal
-              isOpen={table_isOpen}
-              onOpen={table_onOpen}
-              onOpenChange={table_onOpenChange}
-            />
-          </div>
-        </>
-      )}
-    </div>
+    </>
   );
 };
 
--- components/EzSelect/index.tsx
+++ components/EzSelect/index.tsx
@@ -27,67 +27,80 @@ const EzSelect: React.FC = () => {
   );
 
   return (
-    <div className="flex flex-col max-w-[1280px] justify-center w-full">
-      <div className="flex flex-col mb-5 border-b-1 py-5">
-        <div className="mb-2 sectionTitle">
-          <span>Select Page layout direction and number.</span>
+    <>
+      <div className="flex flex-col max-w-[1380px] justify-center w-full">
+        <div className="flex flex-col mb-5 border-b-1 py-5 border-neutral-300 border-dashed">
+          <div className="mb-2 sectionTitle">
+            <span>Select Page layout direction and number.</span>
+          </div>
+          <div className="flex gap-3 px-2 mt-2">
+            <Tabs
+              color="success"
+              onSelectionChange={(v: any) =>
+                setBasicLayout((preV) => {
+                  return { ...preV, direction: v };
+                })
+              }
+              className="optTabs"
+            >
+              {directionTypes.map((type) => (
+                <Tab key={type} title={type} />
+              ))}
+            </Tabs>
+            <Tabs
+              color="danger"
+              onSelectionChange={(v: any) =>
+                setBasicLayout((preV) => {
+                  return { ...preV, step: v };
+                })
+              }
+            >
+              <Tab key="1" title="1단" />
+              <Tab key="2" title="2단" />
+              <Tab key="3" title="3단" />
+            </Tabs>
+          </div>
         </div>
-        <div className="flex gap-3">
-          <Tabs
-            color="success"
-            radius="sm"
-            onSelectionChange={(v: any) =>
-              setBasicLayout((preV) => {
-                return { ...preV, direction: v };
-              })
-            }
+        <div className="flex flex-col gap-3 mb-5 border-b-1 pb-5 border-neutral-300 border-dashed">
+          <div className="mb-2 sectionTitle">
+            <span>Enter the Page title and information.</span>
+          </div>
+          <div className="px-3">
+            <dl className="flex flex-row w-full gap-3">
+              <dt className="w-[275px] font-medium">Page Title</dt>
+              <dd className="w-full">
+                <input
+                  type="text"
+                  className="w-full border-1 border-neutral-300 rounded h-[36px]"
+                />
+              </dd>
+            </dl>
+            <dl className="flex flex-row w-full gap-3 mt-2">
+              <dt className="w-[275px] font-medium">Page Description</dt>
+              <dd className="w-full">
+                <textarea className="w-full border-1 border-neutral-300 rounded h-[74px]" />
+              </dd>
+            </dl>
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <div className="mb-2 sectionTitle">
+            <span>Select a tag to enter a section.</span>
+          </div>
+          {Array.from({ length: Number(basicLayout.step) }, (_, index) => (
+            <TagStep key={index} itemKey={index} />
+          ))}
+        </div>
+        <div className="flex justify-end border-b-1 py-5 px-3 border-neutral-300 border-dashed">
+          <Button
+            color="primary"
+            className="w-[200px] mb-2 border-2 border-sky-800 rounded-md text-lg"
           >
-            {directionTypes.map((type) => (
-              <Tab key={type} title={type} />
-            ))}
-          </Tabs>
-          <Tabs
-            color="danger"
-            radius="sm"
-            onSelectionChange={(v: any) =>
-              setBasicLayout((preV) => {
-                return { ...preV, step: v };
-              })
-            }
-          >
-            <Tab key="1" title="1단" />
-            <Tab key="2" title="2단" />
-            <Tab key="3" title="3단" />
-          </Tabs>
-          <Button color="primary">OK</Button>
+            OK
+          </Button>
         </div>
       </div>
-      <div className="flex flex-col gap-3 mb-5 border-b-1 pb-5">
-        <div className="mb-2 sectionTitle">
-          <span>Enter the Page title and information.</span>
-        </div>
-        <dl className="flex flex-row w-full gap-3">
-          <dt className="w-[220px]">Page Title</dt>
-          <dd className="w-full">
-            <input type="text" className="w-full border-1 rounded-sm" />
-          </dd>
-        </dl>
-        <dl className="flex flex-row w-full gap-3">
-          <dt className="w-[220px]">Page Description</dt>
-          <dd className="w-full">
-            <textarea className="w-full border-1 rounded-sm" />
-          </dd>
-        </dl>
-      </div>
-      <div className="flex flex-col gap-3">
-        <div className="mb-2 sectionTitle">
-          <span>Select a tag to enter a section.</span>
-        </div>
-        {Array.from({ length: Number(basicLayout.step) }, (_, index) => (
-          <TagStep key={index} itemKey={index} />
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
