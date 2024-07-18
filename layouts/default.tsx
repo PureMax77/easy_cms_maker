ae4f6e6d4541f5b73c7431e3d7d1f77b34abd01b
윤효정 <hjyoon@coinplug.com>
2024-07-18 10:35:50 +0900

    :sparkles: feat: head 이동 및 title 삽입

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,19 +1,25 @@
+import Head from "next/head";
 import { Header } from "./Header";
 
 export default function DefaultLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-    <div className="light">
-      <main
-        className={`bg-white w-full min-h-screen h-fit min-w-[1280px] overflow-auto`}
-      >
-        <Header />
-        <div className="px-20">{children}</div>
-        {/* <Footer /> */}
-      </main>
-    </div>
+  return (
+    <>
+      <Head>
+        <title>EZCodeMaker</title>
+      </Head>
+      <div className="light">
+        <main
+          className={`bg-white w-full min-h-screen h-fit min-w-[1280px] overflow-auto`}
+        >
+          <Header />
+          <div className="px-20">{children}</div>
+          {/* <Footer /> */}
+        </main>
+      </div>
+    </>
   );
 }
