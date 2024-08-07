3fbaf0ad827595e795dd765c4ae1230b087e974f
ertr <ertr@coinplug.com>
2024-08-07 14:42:45 +0900

    template1~4 완성

--- components/EzSelect/SampleTemplate.tsx
+++ components/EzSelect/SampleTemplate.tsx
@@ -9,7 +9,9 @@ import {
   BasicDirectionTypes,
   EzListLayoutTypes,
   EzTagTypes,
+  FormItemType,
   IBasicLayout,
+  IFormOptions,
   IListOptions,
   IPageBasicInfo,
   ITableOptions,
@@ -38,6 +40,131 @@ const templateData1: IListOptions[] = [
     isIcon: false,
     isImage: true,
     isDragDrop: false,
+  },
+];
+
+const templateData2: IFormOptions[] = [
+  {
+    kind: EzTagTypes.FORM,
+    title: "",
+    items: 7,
+    itemsContents: [
+      {
+        label: "상품 대표 이미지",
+        tagType: FormItemType.IMAGE,
+        required: true,
+      },
+      {
+        label: "상품명",
+        tagType: FormItemType.INPUT,
+        required: true,
+      },
+      {
+        label: "상품 설명",
+        tagType: FormItemType.INPUT,
+        required: true,
+      },
+      {
+        label: "API 제공 기능 - 발행",
+        tagType: FormItemType.INPUT,
+        required: true,
+      },
+      {
+        label: "API 제공 기능 - 양도",
+        tagType: FormItemType.INPUT,
+        required: true,
+      },
+      {
+        label: "API 제공 기능 - 소멸",
+        tagType: FormItemType.INPUT,
+        required: true,
+      },
+      {
+        label: "API 제공 기능 - 정보 조회",
+        tagType: FormItemType.INPUT,
+        required: true,
+      },
+    ],
+  },
+];
+
+const templateData3: ITableOptions[] = [
+  {
+    kind: EzTagTypes.TABLE,
+    title: "",
+    samples: 20,
+    columns: 3,
+    isRowClick: false,
+    isPagination: true,
+    isDragDrop: false,
+    columnContents: [
+      {
+        title: "구분",
+        tagType: TableColumnType.TEXT,
+        clickEvent: false,
+      },
+      {
+        title: "제목",
+        tagType: TableColumnType.TEXT,
+        clickEvent: false,
+      },
+      {
+        title: "Date",
+        tagType: TableColumnType.TEXT,
+        clickEvent: false,
+      },
+    ],
+  },
+];
+
+const templateData4: (ITableOptions | IFormOptions)[] = [
+  {
+    kind: EzTagTypes.TABLE,
+    title: "",
+    samples: 20,
+    columns: 3,
+    isRowClick: false,
+    isPagination: true,
+    isDragDrop: false,
+    columnContents: [
+      {
+        title: "신청자",
+        tagType: TableColumnType.TEXT,
+        clickEvent: false,
+      },
+      {
+        title: "Status",
+        tagType: TableColumnType.CHECKBOX,
+        clickEvent: false,
+      },
+      {
+        title: "Date",
+        tagType: TableColumnType.TEXT,
+        clickEvent: false,
+      },
+    ],
+  },
+  {
+    kind: EzTagTypes.FORM,
+    title: "",
+    items: 3,
+    itemsContents: [
+      {
+        label: "신청자",
+        tagType: FormItemType.INPUT,
+        required: true,
+      },
+      {
+        label: "신청 사유",
+        tagType: FormItemType.TEXT,
+        required: true,
+      },
+      {
+        label: "Status",
+        tagType: FormItemType.RADIO,
+        required: true,
+      },
+    ],
   },
 ];
 
@@ -81,7 +208,17 @@ const templateData: ITableOptions[] = [
 ];
 
 // 상품리스트
-const additionalPrompt1 = "* Each element includes title and description";
+const additionalPrompt1 =
+  "* Each element includes title and description\n\t* Create a ‘Create Product’ button to the 'flex-row-reverse' between the table and description";
+
+const additionalPrompt2 =
+  "* Add a preview sample image at the top of the image tag\n\t* Add a Save Changes button at the bottom";
+
+const additionalPrompt3 =
+  "* Add a 'Create Notice' button to the 'flex-row-reverse' between the table and description\n\t* Apply 'flex-grow' classname to the '제목' column and 'w-1/6' to the remaining columns\n\t* Express table borders only between rows";
+
+const additionalPrompt4 =
+  "* Add the text ‘Approval’ to the right of the checkbox in table\n\t* Radio consists of two options: 'Disapproval' and 'Approval' in form\n\t* Add a ‘Save Changes’ button at the bottom of the form.";
 
 export default function SampleTemplate() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -97,9 +234,21 @@ export default function SampleTemplate() {
       description: "WEB2X가 제공하는 블록체인 API 상품을 등록 & 관리합니다.",
     },
     {
-      title: "사용자 관리",
-      description: "서비스 사용자를 관리하는 페이지. 블랙리스트 추가 가능.",
+      title: "상품",
+      description: "WEB2X가 제공하는 블록체인 API 상품을 등록 & 관리합니다.",
     },
+    {
+      title: "공지사항",
+      description: "WEB2X의 공지사항과 뉴스를 등록 & 관리합니다.",
+    },
+    {
+      title: "API 사용 신청",
+      description: "WEB2X의 API 사용 신청서 상태를 관리합니다.",
+    },
+    // {
+    //   title: "사용자 관리",
+    //   description: "서비스 사용자를 관리하는 페이지. 블랙리스트 추가 가능.",
+    // },
   ];
 
   const basicLayoutList: IBasicLayout[] = [
@@ -111,9 +260,31 @@ export default function SampleTemplate() {
       direction: BasicDirectionTypes.HORIZONTAL,
       step: "1",
     },
+    {
+      direction: BasicDirectionTypes.HORIZONTAL,
+      step: "1",
+    },
+    {
+      direction: BasicDirectionTypes.HORIZONTAL,
+      step: "2",
+    },
+    // {
+    //   direction: BasicDirectionTypes.HORIZONTAL,
+    //   step: "1",
+    // },
   ];
-  const templateDataList: SectionListType[] = [templateData1, templateData];
-  const additionalPromptList: any[] = [additionalPrompt1, ""];
+  const templateDataList: SectionListType[] = [
+    templateData1,
+    templateData2,
+    templateData3,
+    templateData4,
+  ];
+  const additionalPromptList: any[] = [
+    additionalPrompt1,
+    additionalPrompt2,
+    additionalPrompt3,
+    additionalPrompt4,
+  ];
 
   const handleOpen = (key: number) => {
     setType(key);
@@ -165,10 +336,21 @@ export default function SampleTemplate() {
             />
             Template3
           </Button>
+          <Button onPress={() => handleOpen(4)}>
+            <Image
+              src={"/images/ic_layout.png"}
+              className="rounded-none"
+              alt="EZCodeMaker"
+              width={24}
+              height={24}
+            />
+            Template4
+          </Button>
         </div>
       </div>
+
       <Modal size={"3xl"} isOpen={isOpen} onClose={onClose}>
-        <ModalContent>
+        <ModalContent className="bg-neutral-100">
           {(onClose) => (
             <>
               <ModalHeader className="flex flex-col gap-1">
@@ -177,8 +359,9 @@ export default function SampleTemplate() {
               <ModalBody>
                 <div className="w-full flex justify-center">
                   <Image
-                    src={"/template1.png"}
-                    alt="template1"
+                    className=" rounded-lg"
+                    src={`/template${type}.png`}
+                    alt={`template${type}`}
                     width={500}
                     height={500}
                   />
